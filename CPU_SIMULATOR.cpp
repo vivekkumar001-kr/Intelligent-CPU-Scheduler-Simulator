861701a377ae4638ef14e21a8412af4471e2409f
vivekkumar001-kr <vivekkr96311@gmail.com>
2025-12-16 15:40:44 +0530

    creating function

--- CPU_SIMULATOR.cpp
+++ CPU_SIMULATOR.cpp
@@ -10,3 +10,9 @@ struct Process {
     int start = -1, finish = -1;
     int waiting = 0, turnaround = 0;
 };
+struct Gantt {
+    int pid, start, finish;
+};
+void print results(const vector<Process> &procs , const vector<Gantt> &g){
+    
+}
