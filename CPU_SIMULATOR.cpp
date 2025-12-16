c883d02ad7fdc079ce02f9562f9117cbc8953428
vivekkumar001-kr <vivekkr96311@gmail.com>
2025-12-16 15:32:56 +0530

    Creating struct

--- CPU_SIMULATOR.cpp
+++ CPU_SIMULATOR.cpp
@@ -5,3 +5,8 @@
 #include <algorithm>
 #include <climits>
 using namespace std;
+struct Process {
+    int pid, arrival, burst, priority;
+    int start = -1, finish = -1;
+    int waiting = 0, turnaround = 0;
+};
