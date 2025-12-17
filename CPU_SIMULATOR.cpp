0f16d57514dcad9ecea872fc2a39701d97c5b8a2
vivekkumar001-kr <vivekkr96311@gmail.com>
2025-12-17 13:20:00 +0530

    CPU scheduling Reault Table

--- CPU_SIMULATOR.cpp
+++ CPU_SIMULATOR.cpp
@@ -14,5 +14,18 @@ struct Gantt {
     int pid, start, finish;
 };
 void print results(const vector<Process> &procs , const vector<Gantt> &g){
-    
+    cout << "\nGantt Chart:\n";
+    for (auto &s : g) {
+        if (s.pid == 0) cout << "[idle:" << s.start << "->" << s.finish << "] ";
+        else cout << "[P" << s.pid << ":" << s.start << "->" << s.finish << "] ";
+    }
+    cout << "\n\n---------------- CPU Scheduling Result Table ----------------\n";
+    cout << left
+         << setw(6) << "PID"
+         << setw(12) << "Arrival"
+         << setw(10) << "Burst"
+         << setw(10) << "Start"
+         << setw(10) << "Finish"
+         << setw(10) << "Waiting"
+         << setw(12) << "Turnaround" << "\n";
 }
